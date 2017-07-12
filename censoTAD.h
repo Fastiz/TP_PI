2f523098a2757a2dd02ce12c385e46adb88250b3
migdiluca <mdiluca@itba.edu.ar>
2017-07-12 06:29:26 +0000

    Update censoTAD.h

--- censoTAD.h
+++ censoTAD.h
@@ -7,7 +7,7 @@ censoADT nuevoCenso();
 	Si no lo pudo agregar porque el codigo de vivienda o el codigo de provincia es invalido retorna 0.
 	Si no hay memoria retorna 0.
 	Si lo puede agregar retorna 1*/
-int ingresarDato (censoADT censo, int codigoVivienda, int codigoProvincia, int edad, int analfabeto, char * nombreDepto);
+int ingresarDato (censoADT censo, unsigned char codigoVivienda, unsigned char codigoProvincia, unsigned char edad, char analfabeto, char * nombreDepto);
 
 /* Almacena los datos procesados del censo.
 	Si no se pudieron crear los archivos o almacernar los datos en los mismos retorna 0
